0d7d14a32f4fdfb43cd72fc41243953504deb3b6
Alkuttt07 <atuer1@ocdsb.ca>
2026-02-04 17:17:29 -0500

    Add files via upload

---
+++ Python projects/002personal information.py
@@ -0,0 +1,4 @@
+n= input("What's your name? ")
+a= input("What's your age? ")
+g= input("What's your gender? ")
+print("Hello,",n,", you are",a,f"years old, and your gender is {g}.", end='')
---
+++ Python projects/003logic.py
@@ -0,0 +1,6 @@
+a = int(input("Please show your first intiger."))
+b= int(input("Please show your second intiger."))
+if a >= 10 and b>=10:
+    print(a*b)
+if a>50 or b>50:
+    print(a%b)
