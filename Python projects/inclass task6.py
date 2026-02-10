567dd36b951dfc42058c967f0c83fa0d9098aa02
Alkuttt07 <atuer1@ocdsb.ca>
2026-02-10 02:42:49 -0500

    Add files via upload

--- Python projects/inclass task6.py
+++ Python projects/inclass task6.py
@@ -1,3 +1,4 @@
+import math
 a=[[],[],[0,1]]
 for x in range(10):
     a[0].append(2*x)
@@ -6,10 +7,7 @@ for y in range(8):
     a[2].append(a[2][-1]+a[2][-2])
 while True:
     if input("do you want a factorial number, if so answer yes") == "yes":
-        b=a[int(input("1.even 2.odd 3.fibonacci"))-1]
-        c=int(b[int(input("which order"))-1])
-        for z in range(c):
-            c*=(c-z)
-        print(c)
+        b=a[int(input("1.even 2.odd 3.fibonacci"))-1][int(input("which order"))-1]
+        print(math.factorial(b))
     else:
         break
