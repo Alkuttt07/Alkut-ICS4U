365e9325f56800ede964bbe4b091adcba7b948d7
Alkuttt07 <atuer1@ocdsb.ca>
2026-02-10 22:13:25 -0500

    Add files via upload

---
+++ Python projects/inclass task7.py
@@ -0,0 +1,30 @@
+a=[1,2,3,4,5,6,7,8,9,0]
+for x in a:
+    print(x)
+#1
+b=[]
+for y in range (20):
+    print(4+2*y)
+#2
+c="Alkuttttt"
+for z in c:
+    print(z)
+#3
+d=[0,1]
+for n in range(8):
+    d.append(d[-1]+d[-2])
+print(d)
+#4
+e=["abcd","zxcv","mnbv"]
+for m in e[0]:
+    print(m)
+#5
+f=[[],[]]
+for n in range(5):
+    f[0].append(2*n)
+    f[1].append(2*n+1)
+print(f)
+#6
+for i in range(6):
+    print(i*"*")
+#7
