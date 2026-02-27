b90f89dd697cf3f31c732eeb3601c07bdbe27fc2
Alkuttt07 <atuer1@ocdsb.ca>
2026-02-26 21:18:40 -0500

    Add files via upload

--- Python projects/inclass task15.py
+++ Python projects/inclass task15.py
@@ -3,11 +3,12 @@ class quadratic_relationship:
         self.a=a
         self.b=b
         self.c=c
-        self.yintercept=c
-    def xintercepts(self):
+    def intercepts(self):
         if self.b**2-4*self.a*self.c<0:
-            return "There is no x intercepts."
+            return f"There is no x intercepts, but y intercept is (0,{self.c})."
         elif self.b**2-4*self.a*self.c==0:
-            return -self.b/(2*self.a)
+            return f"x intercept is ({-self.b/(2*self.a)},0), and y intercept is (0,{self.c})."
         else:
-            return [(-self.b+(self.b**2-4*self.a*self.c)**0.5)/(2*self.a),(-self.b-(self.b**2-4*self.a*self.c)**0.5)/(2*self.a)]
+            return f"x intercepts are ({(-self.b+(self.b**2-4*self.a*self.c)**0.5)/(2*self.a)},0) and ({(-self.b-(self.b**2-4*self.a*self.c)**0.5)/(2*self.a)},0), and y intercept is (0,{self.c})."
+x=quadratic_relationship(int(input("What's the a")),int(input("What's the b")),int(input("What's the c")))
+print(x.intercepts())
