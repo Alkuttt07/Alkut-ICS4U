5088b6e96109176027bdec924f077f8b0f73dbeb
Alkuttt07 <atuer1@ocdsb.ca>
2026-04-20 03:21:19 -0400

    Add files via upload

---
+++ motion.py
@@ -0,0 +1,27 @@
+import pygame
+def motion():
+        for event in pygame.event.get():
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if event.button==1:
+                   pass
+            if event.type==pygame.MOUSEBUTTONUP:
+                if event.button==1:
+                    pass
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_w:
+                    pass
+                if event.key==pygame.K_d:
+                    pass
+                if event.key==pygame.K_a:
+                    pass
+                if event.key==pygame.K_s:
+                    pass
+            if event.type==pygame.KEYUP:
+                if event.key==pygame.K_w:
+                    pass
+                if event.key==pygame.K_d:
+                    pass
+                if event.key==pygame.K_a:
+                    pass
+                if event.key==pygame.K_s:
+                    pass
